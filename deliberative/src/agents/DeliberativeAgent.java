76d0b7cb2fcb393c7a41cb3a8cc588d7c2a63453
Jeremia Oberle <jereoberle@yahoo.de>
2016-10-19 18:57:09 +0200

    bfs

--- deliberative/src/agents/DeliberativeAgent.java
+++ deliberative/src/agents/DeliberativeAgent.java
@@ -1,12 +1,23 @@
 package agents;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import logist.agent.Agent;
 import logist.behavior.DeliberativeBehavior;
+import logist.plan.Action;
+import logist.plan.Action.Delivery;
+import logist.plan.Action.Move;
+import logist.plan.Action.Pickup;
 import logist.plan.Plan;
 import logist.simulation.Vehicle;
+import logist.task.Task;
 import logist.task.TaskDistribution;
 import logist.task.TaskSet;
 import logist.topology.Topology;
+import logist.topology.Topology.City;
+import model.State;
 
 public class DeliberativeAgent implements DeliberativeBehavior {
 
@@ -22,38 +33,35 @@ public class DeliberativeAgent implements DeliberativeBehavior {
 	Agent agent;
 	int capacity;
 
+	public static double costPerKm;
+
 	/* the planning class */
 	Algorithm algorithm;
+
+	boolean isJumboTaskSet = false;
 
 	@Override
 	public void setup(Topology topology, TaskDistribution td, Agent agent) {
 		this.topology = topology;
 		this.td = td;
 		this.agent = agent;
+		costPerKm = agent.vehicles().get(0).costPerKm();
 
-		// initialize the planner
 		int capacity = agent.vehicles().get(0).capacity();
 		String algorithmName = agent.readProperty("algorithm", String.class, "ASTAR");
 
-		// Throws IllegalArgumentException if algorithm is unknown
 		algorithm = Algorithm.valueOf(algorithmName.toUpperCase());
-
-		// ...
 	}
 
 	@Override
 	public Plan plan(Vehicle vehicle, TaskSet tasks) {
-
 		Plan plan;
 
-		// Compute the plan with the selected algorithm.
 		switch (algorithm) {
 		case ASTAR:
-			// ...
 			plan = aStar(vehicle, tasks);
 			break;
 		case BFS:
-			// ...
 			plan = BFS(vehicle, tasks);
 			break;
 		default:
@@ -64,19 +72,124 @@ public class DeliberativeAgent implements DeliberativeBehavior {
 	}
 
 	private Plan aStar(Vehicle vehicle, TaskSet tasks) {
-
 		return null;
 	}
 
 	private Plan BFS(Vehicle vehicle, TaskSet tasks) {
+		List<State> queue = new ArrayList<State>();
+		// add root
+		queue.add(new State(tasks, TaskSet.noneOf(tasks), vehicle.getCurrentCity(), 0, vehicle.capacity(), null));
 
-		return null;
+		State bestFinalState = new State(null, null, null, Integer.MAX_VALUE, 0, null);
+
+		int nbStates = 1;
+		State newNeighbor;
+		State current;
+		while (!queue.isEmpty() && bestFinalState.getCost() == Integer.MAX_VALUE) {
+			System.out.println("in while");
+			current = queue.get(0);
+			queue.remove(0);
+			// add new neighbor states
+
+			// pick up a new task if possible
+			for (Task t : current.getAvailableTasks()) {
+				newNeighbor = current.clone();
+				if (newNeighbor.pickUpTask(t)) {
+					// task was picked up
+					if (newNeighbor.getCost() < bestFinalState.getCost()) {
+						newNeighbor.setParentState(current);
+						queue.add(newNeighbor);
+						nbStates++;
+					}
+				}
+			}
+			// deliver one of our picked up tasks
+			for (Task t : current.getPickedUpTasks()) {
+				newNeighbor = current.clone();
+				if (newNeighbor.deliverTask(t)) {
+					// task was delivered
+					if (newNeighbor.getCost() < bestFinalState.getCost()) {
+						newNeighbor.setParentState(current);
+						if (newNeighbor.getAvailableTasks().isEmpty() && newNeighbor.getPickedUpTasks().isEmpty()) {
+							// we are in a final state s we don't need to add it
+							// to
+							// the queue
+							// System.out.println("final state");
+							if (newNeighbor.getCost() < bestFinalState.getCost()) {
+								bestFinalState = newNeighbor;
+							}
+						} else {
+							queue.add(newNeighbor);
+							nbStates++;
+						}
+					}
+				}
+			}
+		}
+		System.out.println("While loop done after " + nbStates);
+		current = bestFinalState;
+		// create actions
+		List<Action> actions = new ArrayList<Action>();
+		do {
+			Task t;
+			if (current.hasPickedUpTaskInCurrentCity()) {
+				// find task that has been picked up in the current.city
+				t = (Task) TaskSet
+				        .intersectComplement(current.getParentState().getAvailableTasks(), current.getAvailableTasks())
+				        .toArray()[0];
+				actions.add(new Pickup(t));
+			} else {
+				// find task that has been delivered in the current.city
+				t = (Task) TaskSet
+				        .intersectComplement(current.getParentState().getPickedUpTasks(), current.getPickedUpTasks())
+				        .toArray()[0];
+				actions.add(new Delivery(t));
+			}
+			actions.addAll(createReverseMoves(current.getParentState().getCurrentCity(), current.getCurrentCity()));
+
+			current = current.getParentState();
+		} while (current != null && current.getParentState() != null);
+		Collections.reverse(actions);
+		return new Plan(vehicle.getCurrentCity(), actions);
+	}
+
+	// private List<Action> getActionsForState(State s) {
+	// List<Action> actions = new ArrayList<Action>();
+	// TaskSet availableTasks =
+	// TaskSet.intersectComplement(s.getParentState().getAvailableTasks(),
+	// s.getAvailableTasks());
+	// if (availableTasks.isEmpty()) {
+	// TaskSet pickedUpTasks =
+	// TaskSet.intersectComplement(s.getParentState().getPickedUpTasks(),
+	// s.getPickedUpTasks());
+	// if (pickedUpTasks.isEmpty()) {
+	// System.out.println("There is a problem, no diff between current and
+	// dad");
+	// } else {
+	// // we delivered a package
+	// Task t = (Task) pickedUpTasks.toArray()[0];
+	// if()
+	// }
+	// } else {
+	// // we picked up a package
+	// }
+	// return actions;
+	// }
+
+	private List<Action> createReverseMoves(City source, City destination) {
+		List<Action> moves = new ArrayList<Action>();
+		if (!source.equals(destination)) {
+			for (City city : source.pathTo(destination)) {
+				moves.add(new Move(city));
+			}
+			Collections.reverse(moves);
+		}
+		return moves;
 	}
 
 	@Override
 	public void planCancelled(TaskSet carriedTasks) {
 		// TODO Auto-generated method stub
-
 	}
 
 }
--- deliberative/src/model/State.java
+++ deliberative/src/model/State.java
@@ -1,22 +1,25 @@
 package model;
 
-import java.util.List;
-
+import agents.DeliberativeAgent;
 import logist.task.Task;
+import logist.task.TaskSet;
 import logist.topology.Topology.City;
 
-public class State {
+public class State implements Cloneable {
 
-	private List<Task> availableTasks;
-	private List<Task> pickedUpTasks;
+	private TaskSet availableTasks;
+	private TaskSet pickedUpTasks;
 
 	private City currentCity;
 
 	private double cost;
 	private int remainingCapacity;
 
-	public State(List<Task> availableTasks, List<Task> pickedUpTasks, City currentCity, double cost,
-			int remainingCapacity) {
+	private State parentState;
+	private boolean pickedUpTaskInCurrentCity;
+
+	public State(TaskSet availableTasks, TaskSet pickedUpTasks, City currentCity, double cost, int remainingCapacity,
+	        State parentState) {
 		super();
 		this.availableTasks = availableTasks;
 		this.pickedUpTasks = pickedUpTasks;
@@ -26,11 +29,11 @@ public class State {
 
 	}
 
-	public List<Task> getAvailableTasks() {
+	public TaskSet getAvailableTasks() {
 		return availableTasks;
 	}
 
-	public List<Task> getPickedUpTasks() {
+	public TaskSet getPickedUpTasks() {
 		return pickedUpTasks;
 	}
 
@@ -47,34 +50,125 @@ public class State {
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
+	public State clone() {
+		return new State(availableTasks.clone(), pickedUpTasks.clone(), currentCity, cost, remainingCapacity,
+		        parentState);
+	}
+
+	public boolean pickUpTask(Task t) {
+		// cannot accept task
+		if (remainingCapacity < t.weight) {
+			return false;
+		}
+		if (availableTasks.remove(t)) {
+			if (pickedUpTasks.add(t)) {
+				cost += currentCity.distanceTo(t.pickupCity) * DeliberativeAgent.costPerKm;
+				remainingCapacity -= t.weight;
+				currentCity = t.pickupCity;
+				pickedUpTaskInCurrentCity = true;
+				return true;
+			} else {
+				System.out.println("task " + t + " already picked up!");
+				System.exit(-1);
+			}
+		} else {
+			System.out.println("task " + t + " not available!");
+			System.exit(-1);
+		}
+		return false;
+	}
+
+	public boolean deliverTask(Task t) {
+		if (pickedUpTasks.remove(t)) {
+			cost += currentCity.distanceTo(t.deliveryCity) * DeliberativeAgent.costPerKm;
+			remainingCapacity += t.weight;
+			currentCity = t.deliveryCity;
+			pickedUpTaskInCurrentCity = false;
 			return true;
-		if (obj == null)
+		} else {
+			System.out.println("task " + t + " has not been picked up!");
+			System.exit(-1);
+		}
+		return false;
+	}
+
+	public void setParentState(State parent) {
+		parentState = parent;
+	}
+
+	public State getParentState() {
+		return parentState;
+	}
+
+	@Override
+	public String toString() {
+		return "S(" + currentCity + ", " + getAvailableTasks().size() + ", " + getPickedUpTasks().size() + ")";
+	}
+
+	// @Override
+	// public String toString() {
+	// return "S(" + currentCity + ", " + getAvailableTasks() + ", " +
+	// getPickedUpTasks() + ")";
+	// }
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()) {
 			return false;
+		}
 		State other = (State) obj;
 		if (availableTasks == null) {
-			if (other.availableTasks != null)
+			if (other.availableTasks != null) {
 				return false;
-		} else if (!availableTasks.equals(other.availableTasks))
+			}
+		} else if (!availableTasks.equals(other.availableTasks)) {
 			return false;
-		if (Double.doubleToLongBits(cost) != Double.doubleToLongBits(other.cost))
+		}
+		if (Double.doubleToLongBits(cost) != Double.doubleToLongBits(other.cost)) {
 			return false;
-		if (remainingCapacity != other.remainingCapacity)
-			return false;
+		}
 		if (currentCity == null) {
-			if (other.currentCity != null)
+			if (other.currentCity != null) {
 				return false;
-		} else if (!currentCity.equals(other.currentCity))
+			}
+		} else if (!currentCity.equals(other.currentCity)) {
 			return false;
+		}
+		if (parentState == null) {
+			if (other.parentState != null) {
+				return false;
+			}
+		} else if (!parentState.equals(other.parentState)) {
+			return false;
+		}
 		if (pickedUpTasks == null) {
-			if (other.pickedUpTasks != null)
+			if (other.pickedUpTasks != null) {
 				return false;
-		} else if (!pickedUpTasks.equals(other.pickedUpTasks))
+			}
+		} else if (!pickedUpTasks.equals(other.pickedUpTasks)) {
 			return false;
+		}
+		if (remainingCapacity != other.remainingCapacity) {
+			return false;
+		}
 		return true;
 	}
 
+	/**
+	 * @return the pickedUpTaskInCurrentCity
+	 */
+	public boolean hasPickedUpTaskInCurrentCity() {
+		return pickedUpTaskInCurrentCity;
+	}
 }
